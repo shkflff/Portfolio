fd84d7a113b71783203779296255f11650f2070b
shkflff <shripadj634@gmail.com>
2026-05-31 00:35:43 +0530

    Update project-images.js

--- project-images.js
+++ project-images.js
@@ -11,4 +11,13 @@ window.PROJECT_IMAGES = [
     src: "assets/projects/WhatsApp Image 2026-05-19 at 6.30.45 PM.jpeg",
     alt: "Kitchen interior project render"
   }
+  ,
+  {
+    src: "assets/projects/Akash Navi 1.jpg",
+    alt: "Kitchen interior project render"
+  },
+  {
+    src: "assets/projects/Akash Navi.jpg",
+    alt: "Kitchen interior project render"
+  }
 ];
